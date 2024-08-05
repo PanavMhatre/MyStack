bda94d465c8a79565a7e034e246d3e594ac481a6
PanavMhatre <115958322+PanavMhatre@users.noreply.github.com>
2024-08-05 12:41:11 -0500

    Add files via upload

---
+++ My Stack/src/Runner.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Runner {
+	public static void main(String[] args) {
+//		//1
+//		System.out.println(StackProbs.makeStack(new int[] {1,2,3,4}));
+//		//2 
+//		System.out.println(StackProbs.doubleUp(StackProbs.makeStack(new int[] {1,3,5,0,-1})));
+//		//3
+//		System.out.println(StackProbs.posAndNeg(StackProbs.makeStack(new int[] {9,-4,3,-1,0,-6})));
+//		//4
+//		System.out.println(StackProbs.shiftByN(StackProbs.makeStack(new int[] {7,23,-7,0,22,-8,4,5}),3));
+//		//5
+//		System.out.println(StackProbs.reverseVowels(("hello how are you")));
+//		//6
+		System.out.println(StackProbs.bracketBalance("(([()])))"));
+		System.out.println(StackProbs.bracketBalance("([()[]()])()"));
+		System.out.println("([()[]()])()".length());
+		
+		
+	}
+	
+	
+
+}
